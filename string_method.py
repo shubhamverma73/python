542d523ede70cd7c626a4690dc106025b4fab259
Shubham <shubhamkrverma73@gmail.com>
2026-02-09 21:59:47 +0530

    added new methods

--- string_method.py
+++ string_method.py
@@ -7,3 +7,11 @@ print(string.title()) # Shubham, Make frist letter capital and rest small
 print(string.capitalize()) # Shubham, Make frist letter capital and rest small
 print(string.count("h")) # 2, count the number of times h is present in string
 print(string.find("h")) # 2, find the index of first occurrence of h
+
+# Excersise - Take name from user and one character, print length of name and count of that character in name either in lower case or upper caseone
+'''
+name = input("What is your name? ")
+one_character = input("Input one character: ")
+print(len(name))
+print(name.lower().count(one_character.lower()))
+'''
---
+++ strip_method.py
@@ -0,0 +1,4 @@
+name = "    Shubham     "
+dots = "..............."
+print(name + dots) #     Shubham     ...............
+print(name.strip() + dots) # Shubham............... strip() are used to remove white spaces from start and end of string
